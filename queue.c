50a96014b801e0a045aa7637103d210c02c320cd
Oskar Lundström <oslundstrom@gmail.com>
2025-01-14 17:04:58 +0100

    Add enqueue implementation

---
+++ queue.c
@@ -0,0 +1,9 @@
+#include "queue.h"
+#include "stdio.h"
+
+int queue_enqueue(Queue *queue, const void *data) {
+	printf("The pointer is %p\n", data);
+	printf("The data is %i\n", *(int *)data); // First cast void pointer to int pointer, then deref
+	int result = list_ins_next(queue, NULL, data);
+	return result;
+}
