d7e918fd4af4851c42fea44bfb548aff0d50b33d
Oskar Lundström <oslundstrom@gmail.com>
2025-01-14 18:56:16 +0100

    Enqueue was done from the wrong direction

--- queue.c
+++ queue.c
@@ -4,11 +4,12 @@
 int queue_enqueue(Queue *queue, const void *data) {
 	printf("The pointer is %p\n", data);
 	printf("The data is %i\n", *(int *)data); // First cast void pointer to int pointer, then deref
-	int result = list_ins_next(queue, NULL, data);
+	int result = list_ins_next(queue, queue->tail, data);
 	return result;
 }
 
 int queue_dequeue(Queue *queue, void **data) {
+	/* Remove from head */
 	int result = list_rem_next(queue, NULL, data);
 	return result;
 }
